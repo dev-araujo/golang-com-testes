fbed93ab0fd49057e247d67844f0cf99b059fc25
dev-araujo <theriancirculo@protonmail.com>
2023-11-25 16:34:27 -0300

    feat: structs, methods & interfaces 01

--- perimetro/perimetro.go
+++ perimetro/perimetro.go
@@ -1,17 +1,30 @@
 package main
 
+import "math"
+
+type Forma interface {
+	Area() float64
+}
+
 type Retangulo struct {
 	Largura float64
 	Altura  float64
 }
 
+func (r Retangulo) Area() float64 {
+	return r.Largura * r.Altura
+}
+
 func Perimetro(retangulo Retangulo) float64 {
 	return 2 * (retangulo.Largura + retangulo.Altura)
-
 }
 
-func Area(retangulo Retangulo) float64 {
-	return retangulo.Largura * retangulo.Altura
+type Circulo struct {
+	Raio float64
 }
 
-// Nosso próximo requisito é escrever uma função Area para círculos.
+func (c Circulo) Area() float64 {
+	return math.Pi * c.Raio * c.Raio
+}
+
+// Refatoração adicional
--- perimetro/perimetro_test.go
+++ perimetro/perimetro_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestPerimetro(t *testing.T) {
 	retangulo := Retangulo{10.0, 10.0}
@@ -8,18 +10,27 @@ func TestPerimetro(t *testing.T) {
 	esperado := 40.0
 
 	if resultado != esperado {
-		t.Errorf("resultado %.2f esperado %.2f", resultado, esperado)
+		t.Errorf("resultado %.2f, esperado %.2f", resultado, esperado)
 	}
-
 }
 
 func TestArea(t *testing.T) {
-	retangulo := Retangulo{12.0, 6.0}
-	resultado := Area(retangulo)
-	esperado := 72.0
+	verificaArea := func(t *testing.T, forma Forma, esperado float64) {
+		t.Helper()
+		resultado := forma.Area()
 
-	if resultado != esperado {
-		t.Errorf("resultado %.2f esperado %.2f", resultado, esperado)
+		if resultado != esperado {
+			t.Errorf("resultado %.2f, esperado %.2f", resultado, esperado)
+		}
 	}
 
+	t.Run("retângulos", func(t *testing.T) {
+		retangulo := Retangulo{12.0, 6.0}
+		verificaArea(t, retangulo, 72.0)
+	})
+
+	t.Run("círculos", func(t *testing.T) {
+		circulo := Circulo{10}
+		verificaArea(t, circulo, 314.1592653589793)
+	})
 }
